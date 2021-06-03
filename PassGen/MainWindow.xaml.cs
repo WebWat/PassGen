d7712326c5bf7e0a681c2df926701c458e62da1f
Serega <seregaplyaskin8@gmail.com>
2021-06-03 21:38:28 +0900

    Add project files.

---
+++ PassGen/MainWindow.xaml.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace PassGen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private StringBuilder Symbols = new StringBuilder(string.Empty);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region Button_Events
+        private void Generate(object sender, RoutedEventArgs e)
+        {
+            if (PasswordLength.Text.Length == 0)
+            {
+                ShowErrorMessage("Укажите длину пароля");
+                return;
+            }
+            else if (int.Parse(PasswordLength.Text) < 5 || int.Parse(PasswordLength.Text) > 60)
+            {
+                ShowErrorMessage("Длина пароля 5-60");
+                return;
+            }
+            else if (Symbols.Length == 0)
+            {
+                ShowErrorMessage("Настройте генератор");
+                return;
+            }
+
+            //Code from:
+            //https://stackoverflow.com/a/54471736
+            /********************************************/
+            var result = new StringBuilder(int.Parse(PasswordLength.Text));
+
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                int count = (int)Math.Ceiling(Math.Log(Symbols.Length, 2) / 8.0);
+
+                int offset = BitConverter.IsLittleEndian ? 0 : sizeof(uint) - count;
+
+                int max = (int)(Math.Pow(2, count * 8) / Symbols.Length) * Symbols.Length;
+
+                byte[] uintBuffer = new byte[sizeof(uint)];
+
+                while (result.Length < int.Parse(PasswordLength.Text))
+                {
+                    rng.GetBytes(uintBuffer, offset, count);
+
+                    uint num = BitConverter.ToUInt32(uintBuffer, 0);
+
+                    if (num < max)
+                    {
+                        result.Append(Symbols[(int)(num % Symbols.Length)]);
+                    }
+                }
+            }
+            /********************************************/
+
+            Password.Text = result.ToString();
+        }
+
+        private void Copy(object sender, RoutedEventArgs e)
+        {
+            if (Password.Text.Length == 0)
+            {
+                ShowErrorMessage("Поле пустое");
+                return;
+            }
+
+            Clipboard.SetText(Password.Text);
+        }
+        #endregion
+
+        #region TextBox_events
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            //Only read the numbers
+            if (!int.TryParse(e.Text, out _))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void TextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            //Remove the reading of the space
+            if (e.Key == Key.Space)
+            {
+                e.Handled = true;
+            }
+        }
+        #endregion
+
+        #region CheckBox_Events
+        private void Uppercase_Checked(object sender, RoutedEventArgs e)
+        {
+            Symbols.Append(SymbolsConstants.Uppercase);
+        }
+
+        private void Uppercase_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Symbols = Symbols.Replace(SymbolsConstants.Uppercase, string.Empty);
+        }
+
+
+        private void Lowercase_Checked(object sender, RoutedEventArgs e)
+        {
+            Symbols.Append(SymbolsConstants.Lowercase);
+        }
+
+        private void Lowercase_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Symbols = Symbols.Replace(SymbolsConstants.Lowercase, string.Empty);
+        }
+
+
+        private void Numbers_Checked(object sender, RoutedEventArgs e)
+        {
+            Symbols.Append(SymbolsConstants.Numbers);
+        }
+
+        private void Numbers_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Symbols = Symbols.Replace(SymbolsConstants.Numbers, string.Empty);
+        }
+
+
+        private void Special_Checked(object sender, RoutedEventArgs e)
+        {
+            Symbols.Append(SymbolsConstants.Special);
+        }
+
+        private void Special_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Symbols = Symbols.Replace(SymbolsConstants.Special, string.Empty);
+        }
+        #endregion
+
+        private void ShowErrorMessage(string text) =>
+            MessageBox.Show(text, null, MessageBoxButton.OK, MessageBoxImage.Error);
+    }
+}
---
+++ PassGen/SymbolsConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace PassGen
+{
+    public class SymbolsConstants
+    {
+        public const string Uppercase = "QWERTYUIOPASDFGHJKLZXCVBNM";
+        public const string Lowercase = "qwertyuiopasdfghjklzxcvbnm";
+        public const string Numbers = "0123456789";
+        public const string Special = "[];,./_{}:<>?~!@#$%^&()-=+№*";
+    }
+}
